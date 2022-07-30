576ca55f82d0c6e44ec98dc516e6be3c4e29fd2c
Juan Carlos Muñoz Ramos <juankimr@gmail.com>
2022-07-30 12:15:24 +0200

    ADD Rename settings folder

--- coolebra/app-config/settings.py
+++ coolebra/app-config/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'coolebra.urls'
+ROOT_URLCONF = 'app-config.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'coolebra.wsgi.application'
+WSGI_APPLICATION = 'app-config.wsgi.application'
 
 
 # Database
